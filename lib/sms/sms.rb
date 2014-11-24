12e69d11e339a31c0571f58c53ff6904ad752c57
Kevin.Xu <cming.xu@gmail.com>
2014-11-24 16:49:56 +0800

    [ADDED] sms message sending service added

---
+++ lib/sms/sms.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+
+module Sms
+  class Sms
+    def self.send_sms(opts)
+      response =HTTParty.post("http://223.4.21.214:8180/Service.asmx/SendMessage",
+                    "Id=300&Name=payaso&Psw=123456&Message=HELLOWORLD&Phone=18611184702&Timestamp=0")
+
+
+    end
+  end
+end
