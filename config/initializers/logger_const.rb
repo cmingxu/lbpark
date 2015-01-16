10b541f3c87faaa6df07d2d5741f48dd1bb367ab
Kevin.Xu <cming.xu@gmail.com>
2015-01-16 13:14:43 +0800

    [ADDED] constants added

---
+++ config/initializers/logger_const.rb
@@ -0,0 +1,8 @@
+PAYMENT_LOGGER  = Logger.new Rails.root.to_s + "/log/payment.log"
+BC_LOGGER  = Logger.new Rails.root.to_s + "/log/bc.log"
+BG_LOGGER  = Logger.new Rails.root.to_s + "/log/bg.log"
+FB_LOGGER  = Logger.new Rails.root.to_s + "/log/finish_bid.log"
+MAIL_LOGGER  = Logger.new Rails.root.to_s + "/log/mail.log"
+SMS_LOGGER  = Logger.new Rails.root.to_s + "/log/sms.log"
+
+
