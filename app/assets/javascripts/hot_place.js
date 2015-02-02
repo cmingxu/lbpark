a31ab709665d6fcd45c987b328dd943366e704d1
kevin xu <cming.xu@gmail.com>
2015-02-02 10:47:10 +0800

    [MOD] data structure improvement

--- app/assets/javascripts/hot_place.js
+++ app/assets/javascripts/hot_place.js
@@ -3,32 +3,34 @@
 
 function hotPlaceSearch(keywords) {
   AMap.service(["AMap.Autocomplete"], function() { 
-        autocomplete = new AMap.Autocomplete({
-            type: 1000,
-            city: "010"
+    if("" == keywords){
+      $("#autocomplete_list").hide().empty();
+    }
+    autocomplete = new AMap.Autocomplete({
+      type: 1000,
+      city: "010"
+    });
+    autocomplete.search(keywords, function(status, result){
+      //根据服务请求状态处理返回结果
+      if(status=='complete') {
+        $("#autocomplete_list").show().empty();
+        result.tips.forEach(function (place) {
+          $("#autocomplete_list").append("<li><a href='/mobile/map?id=" + place.id + "'>" + place.name + "<span> - " + place.district +"</span>" + "</a></li>");
         });
-        autocomplete.search(keywords, function(status, result){
-            //根据服务请求状态处理返回结果
-            if(status=='complete') {
-              $("#autocomplete_list").show().empty();
-              result.tips.forEach(function (place) {
-                $("#autocomplete_list").append("<li><a href='/mobile/map?id=" + place.id + "'>" + place.name + "<span> - " + place.district +"</span>" + "</a></li>");
-              });
-            }
-            if(status=='no_data') {
-              console.log("没找到");
-            }
-            else {
-                console.log(result);
-            }
-        });
-});
+      }
+      if(status=='no_data') {
+        console.log("没找到");
+      }
+      else {
+        console.log(result);
+      }
+    });
+  });
 }
 
 $(document).ready(function () {
   var latest_value = $("#search_input");
-  ["keyup", "change"].forEach(function (event, index) {
-
+  ["keyup", "change", "blur"].forEach(function (event, index) {
     $("#search_input").on(event, function () {
       if(latest_value != $("#search_input").val()){
         latest_value = $("#search_input").val();
