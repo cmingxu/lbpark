461a61061755480432a4c5dc25fc5ff837285341
kevin xu <cming.xu@gmail.com>
2015-02-05 00:09:29 +0800

    [MOD] missing close }

--- app/assets/javascripts/vendor.js
+++ app/assets/javascripts/vendor.js
@@ -4,9 +4,9 @@
 
 
 
-$(document).ready( function () { 
+$(document).ready( function () {
   $("a").click(function () {
-    $.post(config.park_statuses_path, {"status": Math.floor((Math.random() * 3)));
+    $.post(config.park_statuses_path, {"status": Math.floor((Math.random() * 3))});
   });
   }
 );
