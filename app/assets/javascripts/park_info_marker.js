5f6fd3ff1865c3c95cca87220f93fe4b80dde4c0
kevin xu <cming.xu@gmail.com>
2015-02-04 11:44:14 +0800

    [MOD] nav icon

--- app/assets/javascripts/park_info_marker.js
+++ app/assets/javascripts/park_info_marker.js
@@ -4,7 +4,6 @@ LB.park_info_marker = function (park) {
   var busy_status = ["green", "orange", "red", "blue"];
   var classes = ["lb_marker"];
   classes.push("marker_" + busy_status[park.busy_status]);
-  return '<div class="' + classes.join(' ') +'">' + 
-   '<span class="rmb_mark">￥</span>' +
+  return '<div class="' + classes.join(' ') +'">' +
     park.current_price +'</div>'
 }
