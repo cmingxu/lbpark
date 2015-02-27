648ddfe2b9ee74e5984499b4563c95a9f95ff6ac
kevin xu <cming.xu@gmail.com>
2015-02-27 17:48:59 +0800

    [MOD] test change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :wechat, Wechat.config.apiid, Wechat.config.secret
-  provider :wechat, "wxa54c754042737ab9", "20a993eb419d0306c403fc184a391af1"
+  provider :wechat, "wx74e2705cf0a80df8", "7506cf446cb2fa21f7ae0d1b4ae66da4"
 end
