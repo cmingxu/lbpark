03d3eedf6d94cd69435ef0ef65a2c41d0d27419a
kevin xu <cming.xu@gmail.com>
2015-02-28 15:05:54 +0800

    [MOD] key change

--- app/assets/javascripts/where_am_i.js
+++ app/assets/javascripts/where_am_i.js
@@ -15,8 +15,9 @@ LB.where_am_i = function (callback) {
   LB.mapObj.plugin(["AMap.Geolocation"],function(){    //添加浏览器定位服务插件
     　　var geoOptions={
       　　enableHighAccuracy:true,  //是否使用高精度
-      　　timeout: 3000,    //若在指定时间内未定位成功，返回超时错误信息。默认无穷大。
-      　　maximumAge: 1000  //缓存毫秒数。定位成功后，定位结果的保留时间。默认0。
+      　　timeout: 1000,    //若在指定时间内未定位成功，返回超时错误信息。默认无穷大。
+      　　maximumAge: 1000,  //缓存毫秒数。定位成功后，定位结果的保留时间。默认0。
+          convert: true
       　　};
       　　geolocation=new AMap.Geolocation(geoOptions);
       　　AMap.event.addListener(geolocation , 'complete', function (result) {
