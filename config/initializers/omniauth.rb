737314366381e97d87a74af9c97c5e7b94799cfb
kevin xu <cming.xu@gmail.com>
2015-02-28 16:01:03 +0800

    [MOD] spell error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :wechat, Wechat.config.apiid, Wechat.config.secret
-  provider :wechat, "wxa54c754042737ab9", "20a993eb419d0306c403fc184a391af1", { :scope => :snsapi_base }
+  provider :wechat, "wxa54c754042737ab9", "20a993eb419d0306c403fc184a391af1"
 end
