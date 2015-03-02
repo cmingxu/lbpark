ac4bee67856f02a6d4bc3668baa822522213e940
kevin xu <cming.xu@gmail.com>
2015-03-02 13:07:33 +0800

    [MOD] situation when day time end lt day time begin

--- app/models/price_calculator.rb
+++ app/models/price_calculator.rb
@@ -125,7 +125,10 @@ class PriceCalculator
   end
 
   def price_by_day?
-    Time.now.hour >= park.day_time_begin && Time.now.hour < park.day_time_end
+    day_begin = park.day_time_begin
+    day_end   = park.day_time_end
+    day_end   = day_end += 24 if day_end < day_begin # 7:00 - 1:00 Park#16
+    Time.now.hour >= day_begin && Time.now.hour < day_end
   end
 
 
