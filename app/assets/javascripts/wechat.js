545aed2175f3eb513b9f2d20090923242d346a4f
kevin xu <cming.xu@gmail.com>
2015-03-05 19:59:22 +0800

    [ADD] get location from wechat

--- app/assets/javascripts/wechat.js
+++ app/assets/javascripts/wechat.js
@@ -1,8 +1,14 @@
 wx.ready(function () {
-  console.log('wechat ready');
+
+  wx.getLocation({
+    success: function (res) {
+      var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+      var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+      var speed = res.speed; // 速度，以米/每秒计
+      var accuracy = res.accuracy; // 位置精度
+      alert(latitude);
+      alert(longitude);
+    }
+  });
 });
 
-wx.error(function (res) {
-  console.log('errpr');
-  console.log(res);
-});
