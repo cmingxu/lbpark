e0d7f331440c169ba290c86cd4afd74062ba6c9e
kevin xu <cming.xu@gmail.com>
2015-03-05 17:40:50 +0800

    [MOD] wechat api initialization change

--- config/initializers/wechat.rb
+++ config/initializers/wechat.rb
@@ -1,2 +1,2 @@
-$wechat_api = Wechat::Api.new(Wechat.config.appid, Wechat.config.secret, Wechat.config.access_token)
-$vendor_wechat_api = Wechat::Api.new(Wechat.config.vendor_appid, Wechat.config.vendor_secret, Wechat.config.vendor_access_token)
+$wechat_api = Wechat::Api.new(Wechat.config.appid, Wechat.config.secret, Wechat.config.access_token, Wechat.config.js_api_ticket)
+$vendor_wechat_api = Wechat::Api.new(Wechat.config.vendor_appid, Wechat.config.vendor_secret, Wechat.config.vendor_access_token, Wechat.config.vendor_js_api_ticket)
