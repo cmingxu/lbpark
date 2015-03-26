b782043cecf887f1abd67ae346a487b9a866dcab
kevin xu <cming.xu@gmail.com>
2015-03-26 15:48:55 +0800

    [MOD]

--- app/assets/javascripts/auto_nav.js
+++ app/assets/javascripts/auto_nav.js
@@ -14,7 +14,7 @@ LB.auto_nav = function(park) {
         //驾车策略，包括 LEAST_TIME，LEAST_FEE, LEAST_DISTANCE,REAL_TRAFFIC
         policy: AMap.DrivingPolicy.LEAST_TIME
       };
-      MDrive = new AMap.Driving(DrivingOption); //构造驾车导航类 
+      MDrive = new AMap.Driving(DrivingOption); //构造驾车导航类
       //根据起终点坐标规划驾车路线
       MDrive.search(start_xy, end_xy, function(status, result){
         if(status === 'complete' && result.info === 'OK'){
@@ -29,32 +29,32 @@ LB.auto_nav = function(park) {
   //绘制驾车导航路线
   function drivingDrawLine(s) {
     //起点到路线的起点 路线的终点到终点 绘制无道路部分
-    var extra_path1 = new Array();
-    extra_path1.push(start_xy);
-    extra_path1.push(steps[0].path[0]);
-    var extra_line1 = new AMap.Polyline({
-      map: LB.mapObj,
-      path: extra_path1,
-      strokeColor: "#9400D3",
-      strokeOpacity: 0.7,
-      strokeWeight: 4,
-      strokeStyle: "dashed",
-      strokeDasharray: [10, 5]
-    });
+    //var extra_path1 = new Array();
+    //extra_path1.push(start_xy);
+    //extra_path1.push(steps[0].path[0]);
+    //var extra_line1 = new AMap.Polyline({
+      //map: LB.mapObj,
+      //path: extra_path1,
+      //strokeColor: "#9400D3",
+      //strokeOpacity: 0.7,
+      //strokeWeight: 4,
+      //strokeStyle: "dashed",
+      //strokeDasharray: [10, 5]
+    //});
 
-    var extra_path2 = new Array();
-    var path_xy = steps[(steps.length-1)].path;
-    extra_path2.push(end_xy);
-    extra_path2.push(path_xy[(path_xy.length-1)]);
-    var extra_line2 = new AMap.Polyline({
-      map: LB.mapObj,
-      path: extra_path2,
-      strokeColor: "#9400D3",
-      strokeOpacity: 0.7,
-      strokeWeight: 4,
-      strokeStyle: "dashed",
-      strokeDasharray: [10, 5]
-    });
+    //var extra_path2 = new Array();
+    //var path_xy = steps[(steps.length-1)].path;
+    //extra_path2.push(end_xy);
+    //extra_path2.push(path_xy[(path_xy.length-1)]);
+    //var extra_line2 = new AMap.Polyline({
+      //map: LB.mapObj,
+      //path: extra_path2,
+      //strokeColor: "#9400D3",
+      //strokeOpacity: 0.7,
+      //strokeWeight: 4,
+      //strokeStyle: "dashed",
+      //strokeDasharray: [10, 5]
+    //});
 
     var drawpath = new Array(); 
     for(var s=0; s<steps.length; s++) {
@@ -72,8 +72,9 @@ LB.auto_nav = function(park) {
       strokeDasharray: [10, 5]
     });
 
-    LB.auto_nav_lines = [extra_line1, extra_line2, polyline];
-    //LB.mapObj.setFitView();
+    //LB.auto_nav_lines = [extra_line1, extra_line2, polyline];
+    LB.auto_nav_lines = [polyline];
+    LB.mapObj.setFitView();
   }
   driving_route();
 };
