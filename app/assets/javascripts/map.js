979f0018a96dad7c46b5588dc0168b99c7bbefd6
kevin xu <cming.xu@gmail.com>
2015-03-26 16:11:19 +0800

    [MOD]

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -184,6 +184,7 @@ function add_current_position_marker(){
 function back_to_original_marker(){
   $("#back_to_original_marker").click(function () {
     LB.mapObj.panTo(new AMap.LngLat(LB.current_location.lng, LB.current_location.lat));
+    //LB.mapObj.setCenter(new AMap.LngLat(config.default_location.lat, config.default_location.lng));
   });
 }
 
