4f205c7f4f6b7cb8e112e562033cce92b23ba382
kevin xu <cming.xu@gmail.com>
2015-03-27 11:46:42 +0800

    [MOD]

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -193,10 +193,13 @@ $(document).ready(function () {
   FastClick.attach(document.body);
   mapInit();
   $("#nav_button_click_area").click(function (event) {
+    alert('click');
     LB.park_info_state.on_enter_short(LB.park_info_state.get_current_park());
     LB.auto_nav(LB.current_park);
+    alert(LB.current_park);
     event.stopPropagation();
     event.preventDefault();
+    alert('123');
     return;
   });
 
@@ -223,14 +226,11 @@ wx.ready(function () {
       LB.current_location.lng = longitude;
       LB.current_location.lat = latitude;
       if(place_name == ""){ // set center to current location if no searching
-        alert(place_name);
         LB.mapObj.setCenter(new AMap.LngLat(LB.current_location.lng, LB.current_location.lat));
         LB.center = LB.current_location;
-        alert(LB.center);
 
         fetch_parkes(LB.center);
         if(LB.current_position_marker){
-          alert('123');
           LB.current_position_marker.setPosition(new AMap.LngLat(LB.current_location.lng, LB.current_location.lat));
         }else{
           add_current_position_marker();
