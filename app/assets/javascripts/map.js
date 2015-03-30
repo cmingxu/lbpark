4ddee402a3677a9ef5667d9f198cc1fbb90f3ab7
kevin xu <cming.xu@gmail.com>
2015-03-30 17:06:49 +0800

    [MOD]

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -237,7 +237,7 @@ function getLocationAndPanTo() {
   wx.getLocation({
     type: 'gcj02',
     success: function (res) {
-      console.log(res.latitude);
+      alert(res.latitude);
       var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
       var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
       var speed = res.speed; // 速度，以米/每秒计
@@ -250,8 +250,8 @@ function getLocationAndPanTo() {
       }
       LB.current_location.lng = longitude;
       LB.current_location.lat = latitude;
-      console.log(LB.current_location.lng);
-      console.log('111111');
+      alert(LB.current_location.lng);
+      alert('111111');
 
       LB.mapObj.setCenter(new AMap.LngLat(LB.current_location.lng, LB.current_location.lat));
       storeCurrentLocation(longitude, latitude);
