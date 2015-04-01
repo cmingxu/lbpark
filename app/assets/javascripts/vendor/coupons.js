ebc19db26f35541210e7deff7225d46f318d6708
kevin xu <cming.xu@gmail.com>
2015-04-01 16:16:26 +0800

    [MOD]

--- app/assets/javascripts/vendor/coupons.js
+++ app/assets/javascripts/vendor/coupons.js
@@ -4,7 +4,7 @@ wx.ready(function () {
       needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
       success: function (res) {
         var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-        alert(res.resultStr);
+        $toast(res.resultStr);
         window.location.href = "/vendor_coupons/" + result + "/use";
       }
     });
