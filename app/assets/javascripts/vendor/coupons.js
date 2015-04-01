65495df95e8756840ae6d5499633da3524252896
kevin xu <cming.xu@gmail.com>
2015-04-01 16:13:09 +0800

    [MOD]

--- app/assets/javascripts/vendor/coupons.js
+++ app/assets/javascripts/vendor/coupons.js
@@ -1,14 +1,11 @@
 wx.ready(function () {
   $("#scanQRCode").click(function (event) {
     wx.scanQRCode({
-      needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-      scanType: ["qrCode"], // 可以指定扫二维码还是一维码，默认二者都有
+      needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
       success: function (res) {
         var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+        alert(res.resultStr);
         window.location.href = "/vendor_coupons/" + result + "/use";
-      },
-      error: function (res) {
-        $toast("扫码失败， 请反馈给萝卜停车...");
       }
     });
   })
