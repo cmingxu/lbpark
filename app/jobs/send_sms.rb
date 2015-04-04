641cc432963ceb3e62bc46d8cd0ae16bbc3dd6dd
kevin xu <cming.xu@gmail.com>
2015-04-04 15:56:41 +0800

    [MOD]

--- app/jobs/send_sms.rb
+++ app/jobs/send_sms.rb
@@ -7,7 +7,7 @@ class SendSms
   def self.perform(id)
     SMS_LOGGER.debug "entering send_sms #{id}"
     return unless sn = SmsCode.find_by_id(id)
-    result, error_message = SmsSender.send_sms_to_mobile(sn.phone, sn.send_reason, sn.params)
+    result, error_message = ::SmsSender.send_sms_to_mobile(sn.phone, sn.send_reason, sn.params)
     if result
       sn.sent!
       SMS_LOGGER.debug "send to #{sn.phone} with #{sn.params}"
