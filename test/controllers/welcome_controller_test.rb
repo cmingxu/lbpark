c0a5a1a11496dfa28253ccf109442b1fa3fad258
kevin xu <cming.xu@gmail.com>
2015-04-04 12:19:07 +0800

    [MOD]

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
-  test "should get map" do
-    get :map
+  test "should get index" do
+    get :index
     assert_response :success
   end
 
