395173f05c054a641c98e2e22622e3f3549ab40e
kevin xu <cming.xu@gmail.com>
2015-04-16 22:36:31 +0800

    [MOD]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # reset captcha code after each request for security
   after_filter :reset_last_captcha_code!
 
+  before_filter do
+    Rails.logger.debug request.remote_ip
+    Rails.logger.debug "1" * 100
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
