1257fa14e3b3d5fdb6075a0eb837a5b55160bb43
kevin xu <cming.xu@gmail.com>
2015-04-17 15:51:55 +0800

    [MOD]

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -1,6 +1,6 @@
 # required
 WxPay.appid = 'wx97b3f3548f2c5c76'
-WxPay.key = '9498be2f38d2d3cdc831ecce35fbb428759ef187ffcdbb225c043d7474831717'
+WxPay.key = '184eac52da804e4542e0b2ef85b2c609'
 WxPay.mch_id = '1235113502'
 
 # optional - configurations for RestClient timeout, etc.
