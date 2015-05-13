71e688f29fe09f0f2e55922e8d62791102088a12
kevin xu <cming.xu@gmail.com>
2015-05-13 12:44:59 +0800

    MOD

--- app/controllers/api/barriers_controller.rb
+++ app/controllers/api/barriers_controller.rb
@@ -2,12 +2,13 @@ class Api::BarriersController < Api::BaseController
   skip_before_filter :verify_authenticity_token
 
   def event
-    BARRIER_LOGGER.debug params[:req]
-    render :json => {:res => "OK", :op => "OPEN" }
+    BARRIER_LOGGER.debug params
+    render :json => {:res => "OK", :msg => "OPEN" }
+    #render :json => {:res => "FAIL", :msg => "OPEN" }
   end
 
   def heartbeat
-    BARRIER_LOGGER.debug params[:req]
-    render :json => {:res => "OK"}
+    BARRIER_LOGGER.debug params
+    render :json => {:res => "OK", :msg => "", :msg_type => ""}
   end
 end
