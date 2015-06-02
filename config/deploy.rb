c30b125b75d26914ec77f6508179080bc5a48758
kevin xu <cming.xu@gmail.com>
2015-06-02 15:43:28 +0800

    [MOD]

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'lbpark'
-set :repo_url, 'git@github.com:arieso/lbpark.git'
+set :repo_url, 'git@git-arieso:arieso/lbpark.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
